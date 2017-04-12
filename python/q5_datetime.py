43fb051c564c4fbda28921ef69ec9fdeb7f374d9
andrewkruger <andrew.kruger@gmail.com>
2017-04-11 22:23:38 -0500

    Show code used to get answers.

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -4,10 +4,27 @@
 date_start = '01-02-2013'  
 date_stop = '07-28-2015'   
 
+from datetime import datetime
+date_format = "%m-%d-%Y"
+start = datetime.strptime(date_start, date_format)
+stop = datetime.strptime(date_stop, date_format)
+print (stop-start).days
+
 ####b)  
 date_start = '12312013'  
 date_stop = '05282015'  
 
+date_format = "%m%d%Y"
+start = datetime.strptime(date_start, date_format)
+stop = datetime.strptime(date_stop, date_format)
+print (stop-start).days
+
+
 ####c)  
 date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
+date_stop = '14-Jul-2015'
+
+date_format = '%d-%b-%Y'
+start = datetime.strptime(date_start, date_format)
+stop = datetime.strptime(date_stop, date_format)
+print (stop-start).days
