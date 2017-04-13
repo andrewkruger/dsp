ad8242c84f048ad13b7240e2fe25f894481e0ff1
andrewkruger <andrew.kruger@gmail.com>
2017-04-13 17:24:29 -0500

    Answered Q1

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,1 +1,12 @@
-PLACE YOUR CODE HERE
+#Q1)
+import pandas as pd
+df = pd.read_csv('https://raw.githubusercontent.com/andrewkruger/dsp/master/python/faculty.csv')
+deg_dict = {}
+
+for degs in df[' degree']:
+    for deg in degs.lstrip(' ').replace('.','').split(' '):
+        if deg != '0':
+            deg_dict[deg] = deg_dict.get(deg, 0) + 1
+
+print deg_dict
+
