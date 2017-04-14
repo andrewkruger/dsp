4c5be91f95f9f84f1648b0231cc4d8e8875c8660
andrewkruger <andrew@Andrews-Air.attlocal.net>
2017-04-14 14:52:40 -0500

    Answered all questions.

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,4 @@
-PLACE YOUR CODE HERE
+####Q1)
+import pandas as pd
+df = pd.read_csv('https://raw.githubusercontent.com/andrewkruger/dsp/master/python/faculty.csv')
+df[' email'].to_csv('emails.csv', header=False, index = False)
